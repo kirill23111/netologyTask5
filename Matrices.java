a3d02199ec1ad9f60795297469c6622886eb48ac
kirill23111 <110820421+kirill23111@users.noreply.github.com>
2025-04-23 22:16:54 +0200

    Add files via upload

---
+++ Matrices.java
@@ -0,0 +1,48 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+public class Matrices {
+    public static final int SIZE = 8;
+
+    public static void main(String[] args) {
+        int[][] colors = new int[SIZE][SIZE];
+
+        Random random = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                // для случайных значений воспользуемся готовым решением из библиотеки java.util.Random
+                colors[i][j] = random.nextInt(256);
+            }
+        }
+        printMatrix(colors);
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        int[][] rotatedColors = new int[SIZE][SIZE];
+
+        for(int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                rotatedColors[i][j] = colors[SIZE - 1 - j][i];
+            }
+        }
+        printMatrix(rotatedColors);
+    }
+
+    public static void printMatrix(int[][] colors) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                // %4d означает, что мы под каждый номер резервируем 4 знака
+                // (незанятые будут заполнены пробелами)
+                // таким образом, у нас получится ровная таблица
+                System.out.format("%4d", colors[i][j]);
+            }
+            // Переход на следующую строку
+            System.out.println();
+        }
+    }
+
+
+}
