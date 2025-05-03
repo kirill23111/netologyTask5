943593184fc4c125c87b48e96a75962915df225e
kirill23111 <110820421+kirill23111@users.noreply.github.com>
2025-05-03 23:48:01 +0200

    Delete src directory

--- src/taxes/UsnDebCrTax.java
+++
@@ -1,8 +0,0 @@
-package taxes;
-
-public class UsnDebCrTax extends TaxSystem{
-        public int calcTaxFor(int debit, int credit) {
-            int profit = debit - credit;
-            int tax = (int) (profit * 0.15);
-            return Math.max(tax, 0);}
-    }
--- src/taxes/UsnDebit.java
+++
@@ -1,8 +0,0 @@
-package taxes;
-
-public class UsnDebit extends TaxSystem{
-        public int calcTaxFor(int debit, int credit) {
-            return (int) (debit * 0.06);
-        }
-    }
-
