642a8f89f68ff20a430670055e558d4075a56111
kirill23111 <110820421+kirill23111@users.noreply.github.com>
2025-05-04 19:51:18 +0200

    Delete src directory

--- src/SimpleLogger.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDateTime;
-
-public class SimpleLogger implements Logger{
-    @Override
-    public void log(String msg) {
-        System.out.println("[" + LocalDateTime.now() + "]" + " " + msg);
-    }
-}
